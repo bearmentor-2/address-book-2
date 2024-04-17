200e9e79aefcbdd3e28cdf0834f7afe03a919332
M Haidar Hanif <git@mhaidarhanif.com>
2024-04-17 22:01:37 +0700

    feat: create initial contacts data

---
+++ index.js
@@ -0,0 +1,22 @@
+const contacts = [
+  {
+    fullName: "Steven Paul Jobs",
+    nickName: "Steve",
+    email: "steve@apple.com",
+    phone: "+1-23456789",
+    age: 56,
+    isAlive: false,
+    address: "Palo Alto, California, USA",
+  },
+  {
+    fullName: "William Henry Gates III",
+    nickName: "Bill",
+    email: "bill@microsoft.com",
+    phone: "+1-98765432",
+    age: 68,
+    isAlive: true,
+    address: "Seattle, Washington, USA",
+  },
+];
+
+console.log(contacts);
